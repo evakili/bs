88c0f2db352236e5e3653d5c3f88e228fc4e2127
Esmaeil Vakili <vakili@payampardaz.local>
2019-10-30 09:37:33 +0330

    value could be int

--- bs.cpp
+++ bs.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 
-using position_t = int*;
+using value_t = int;
+using position_t = value_t*;
 using difference_t = std::size_t;
 
 difference_t distance(position_t first, position_t last)
@@ -13,7 +14,7 @@ position_t next(position_t pos, difference_t n = 1)
     return pos + n;
 }
 
-position_t lower_bound(position_t first, position_t last, int value)
+position_t lower_bound(position_t first, position_t last, const value_t& value)
 {
     while (last != first) {
         auto middle = next(first, distance(first, last) / 2);
@@ -27,15 +28,15 @@ position_t lower_bound(position_t first, position_t last, int value)
     return first;
 }
 
-position_t binary_search(position_t first, position_t last, int value)
+position_t binary_search(position_t first, position_t last, const value_t& value)
 {
     first = lower_bound(first, last, value);
     return *first == value ? first : last;
 }
 
 int main() {
-    int arr[] = { 1, 3, 4, 6, 7, 9, 12, 54, 200, 1000 };
-    std::size_t len = sizeof(arr) / sizeof(int);
+    value_t arr[] = { 1, 3, 4, 6, 7, 9, 12, 54, 200, 1000 };
+    std::size_t len = sizeof(arr) / sizeof(value_t);
 
     std::cout << binary_search(arr, arr + len, 7) << '\n';
     std::cout << binary_search(arr, arr + len, 54) << '\n';
